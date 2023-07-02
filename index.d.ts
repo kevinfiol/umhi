1b0da723dd897753dbd5d334896569fcbeab3ec1
kevinfiol <me@kevinfiol.com>
2023-07-02 01:37:36 -0400

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,22 @@
-type UmaiNode = string | number | null | undefined;
-type ChildNode = UmaiNode | VNode;
+type UmhiNode = string | number | null | undefined;
+type ChildNode = UmhiNode | VNode;
 
 interface Props {
   [propName: string]: unknown;
 }
 
 interface VNode {
-  m?: number;
+  _cmp?: number;
   tag: string | Component | StatefulComponent;
-  props: Attrs;
-  children: ChildNode[];
+  props?: Props;
+  children?: ChildNode[];
 }
 
-type Component = (props: Props, oldProps?: Props) => Vnode;
-type StatefulComponent = (props: Props) => Component;
-
-/** Creates a virtual DOM node. Can be used to create HTML Element vnodes or consume components. **/
-export function m(tag: string, ...tail: (Props | UmaiNode)[]): VNode;
-
-/** Returns the old virtual DOM node for the current component. **/
-export function retain(): VNode;
-
-/** Memoizes the given component. Components will re-render only if their props change between render cycles. **/
-export function memo(component: Component | StatefulComponent): Component | StatefulComponent;
-
-/** Removes all redraw handlers from the global redraw pool. **/
-export function reset(): void;
+/** Creates a virtual DOM node. Can be used to create HTML Element vnodes. **/
+export function m(tag: string, ...tail: (Props | UmhiNode)[]): VNode;
 
 /** Rerenders all currently mounted applications. **/
 export function redraw(): void;
 
-/** Mounts a Component on a given DOM Node. Returns redraw handler. **/
-export function mount(node: Node, root: Component): () => void;
+/** Mounts a factory function on a given DOM Node. Returns redraw handler. **/
+export function mount(node: Node, root: () => VNode): () => void;
